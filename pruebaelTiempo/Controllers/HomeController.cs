775d86b12726ceda7e9332a6c968e11a6cf453be
David Santiago Vera Vargas <santi.2900@hotmail.com>
2022-12-05 17:39:51 -0500

    Agregar archivos de proyecto.

---
+++ pruebaelTiempo/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pruebaelTiempo.Models;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace pruebaelTiempo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            response datos = new response();
+
+            using (var httpClient = new HttpClient())
+            {
+                var task = httpClient.GetAsync("https://images-api.nasa.gov/search?q=apollo%2011");
+                task.Wait();
+                var result = task.Result.Content.ReadAsStringAsync().Result;
+
+                datos = JsonSerializer.Deserialize<response>(result);
+            }
+
+            datos.collection.items = datos.collection.items.GetRange(0, 10);
+
+            return View(datos);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ pruebaelTiempo/Models/response.cs
@@ -0,0 +1,55 @@
+﻿namespace pruebaelTiempo.Models
+{
+  
+        public class Datum
+        {
+            public string center { get; set; }
+            public string title { get; set; }
+            public List<string> keywords { get; set; }
+            public string nasa_id { get; set; }
+            public DateTime date_created { get; set; }
+            public string media_type { get; set; }
+            public string description { get; set; }
+            public string photographer { get; set; }
+            public string location { get; set; }
+            public List<string> album { get; set; }
+        }
+
+        public class Link
+        {
+            public string href { get; set; }
+            public string rel { get; set; }
+            public string render { get; set; }
+            public string prompt { get; set; }
+        }
+        public class Item
+        {
+            public string href { get; set; }
+            public List<Datum> data { get; set; }
+            public List<Link> links { get; set; }
+        }
+
+        public class Metadata
+        {
+            public int total_hits { get; set; }
+        }
+
+
+
+        public class Collection
+        {
+            public string version { get; set; }
+            public string href { get; set; }
+            public List<Item> items { get; set; }
+            public Metadata metadata { get; set; }
+            public List<Link> links { get; set; }
+        }
+
+        public class response
+        {
+            public Collection collection { get; set; }
+        }
+
+
+    
+}
